8ff0753d6064ced53388cc74bb81c29ccf11916e
Olga Shalakhina <osshalakhina@gmail.com>
2023-05-21 11:14:30 +0300

    update

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,35 +1,15 @@
 import Proj1 from "../assets/projects/proj1.jpg";
 import Proj2 from "../assets/projects/proj2.jpg";
-import Proj3 from "../assets/projects/proj3.jpg";
-import Proj4 from "../assets/projects/proj4.jpg";
 import Proj7 from "../assets/projects/proj7.jpg";
-import Proj8 from "../assets/projects/proj8.jpg";
 
 export const ProjectList = [
   {
-    name: "Portfolio (new)",
+    name: "Portfolio",
     description: "[ React.js, JavaScript, HTML5, SASS ]",
     image: Proj7,
     link: "https://github.com/osshalakhina/react_portfolio",
     demo: "https://kosov.site/",
   },
-
-  // {
-  //   name: "Portfolio",
-  //   description: "[ React.js, HTML5, Tailwind CSS ]",
-  //   image: Proj4,
-  //   link: "https://github.com/osshalakhina/my_portfolio",
-  //   demo: "https://shalakhina.com/",
-  // },
-
-  // {
-  //   name: "FeatureHub.Ai",
-  //   description: "[ React.js, JavaScript, HTML5, SASS ]",
-  //   image: Proj8,
-  //   link: "https://beta.featurehub.ai/",
-  //   demo: "https://beta.featurehub.ai/",
-  // },
-
   {
     name: "Mesto",
     description: "[ JavaScript, HTML5, CSS3 ]",
@@ -44,11 +24,4 @@ export const ProjectList = [
     link: "https://github.com/osshalakhina/russian-travel?",
     demo: "https://osshalakhina.github.io/russian-travel/?",
   },
-  // {
-  //   name: "How To Learn",
-  //   description: "[ HTML5, CSS3 ]",
-  //   image: Proj3,
-  //   link: "https://github.com/osshalakhina/how-to-learn?",
-  //   demo: "https://osshalakhina.github.io/how-to-learn/",
-  // },
 ];
